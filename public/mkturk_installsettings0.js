c20c19b973b2fc8d6783181b4f457bafc701ff38
erik emeric <erik.emeric@gmail.com>
2025-12-16 13:47:06 -0500

    same changes as before

--- public/mkturk_installsettings0.js
+++ public/mkturk_installsettings0.js
@@ -2,24 +2,19 @@
 // Also used for liveplot to identify where to look for behavioral files
 
 // ------ Dropbox WebApp settings ------ 
-
-// MKTURK
-var DBX_CLIENT_ID = "2m9hmvxxxxxxxxx"
-var DBX_REDIRECT_URI_ROOT = "http://localhost:8080/" //local
-// var DBX_REDIRECT_URI_ROOT = "https://sandbox-ce2c5.firebaseapp.com/mkturk/" //firebase
+// PATH & APP KEY from Dropbox Developer Console for
+// MKTURK.HTML & LIVEPLOT.HTML
+var DBX_CLIENT_ID = "2m9hmv7q45kwren"
+var DBX_REDIRECT_URI_ROOT = "https://eeemeric.github.io/mkturk/public"
 
 // ------ Subject settings ------ 
-var subjectlist = [
-"Eliaso","Michaelo"
-];
+var subjectlist = ["ExampleSubject"];
 
 // ------ Save location settings ------
-var DATA_SAVEPATH = "/mkturkfiles/datafiles/"
-var PARAM_DIRPATH = "/mkturkfiles/parameterfiles/subjects/"
-var SOUND_FILEPREFIX = "/mkturkfiles/sounds/au"
+var DATA_SAVEPATH = "/mkturkfolders/datafiles/"
+var PARAM_DIRPATH = "/mkturkfolders/parameterfiles/subjects/"
+var SOUND_FILEPREFIX = "/mkturkfolders/sounds/au"
 
 // ------ Misc. -----------------------
 var ndatafiles2read=5; // todo: change to trials. and use as upper bound (stop reading once you hit the first discrepancy). maybe this is effectively synonymous with mintrials
-var num_preload_images=5; // how long can you/the NHP bother waiting at each imageload? 400 images ~ 30 seconds. Recommended to keep = 0 with good internet connection and automator on
-
-// ------ todo: move into params file -
+var num_preload_images=0; // how long can you/the NHP bother waiting at each imageload? 400 images ~ 30 seconds. Recommended to keep = 0 with good internet connection and automator on
