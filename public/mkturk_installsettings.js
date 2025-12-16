c6bc0b8192128b4e4d7adf9395d622f9f9d70db9
erik emeric <erik.emeric@gmail.com>
2025-12-16 14:11:43 -0500

    another update to DBX_REDIRECT_URI_ROOT

--- public/mkturk_installsettings.js
+++ public/mkturk_installsettings.js
@@ -5,7 +5,7 @@
 // PATH & APP KEY from Dropbox Developer Console for
 // MKTURK.HTML & LIVEPLOT.HTML
 var DBX_CLIENT_ID = "2m9hmv7q45kwren"
-var DBX_REDIRECT_URI_ROOT = "https://dl.dropboxusercontent.com/spa/k79b8ph6lmcr30d/mkturk/public/"
+var DBX_REDIRECT_URI_ROOT = "https://eeemeric.github.io/my_mkturk/mkturk/public/"
 
 // ------ Subject settings ------ 
 var subjectlist = ["ExampleSubject"];
