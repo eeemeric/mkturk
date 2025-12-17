db99f0d83f165ea73c038fab6ec2351da9ac1ca4
erik emeric <erik.emeric@gmail.com>
2025-12-17 18:00:52 -0500

    Update mkturk_installsettings.js

--- mkturk_installsettings.js
+++ mkturk_installsettings.js
@@ -9,7 +9,7 @@ var DBX_CLIENT_ID = "cybuxa7akag6f56" // E_mturk on dropbox
 var DBX_REDIRECT_URI_ROOT = "https://eeemeric.github.io/mkturk/"
 
 // ------ Subject settings ------ 
-var subjectlist = ["ExampleSubject"];
+var subjectlist = ["ExampleSubject","Eliaso"];
 
 // ------ Save location settings ------
 var DATA_SAVEPATH = "/mkturkfolders/datafiles/"
@@ -19,6 +19,7 @@ var SOUND_FILEPREFIX = "/mkturkfolders/sounds/au"
 // ------ Misc. -----------------------
 var ndatafiles2read=5; // todo: change to trials. and use as upper bound (stop reading once you hit the first discrepancy). maybe this is effectively synonymous with mintrials
 var num_preload_images=0; // how long can you/the NHP bother waiting at each imageload? 400 images ~ 30 seconds. Recommended to keep = 0 with good internet connection and automator on
+
 
 
 
